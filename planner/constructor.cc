dcd63ffe09f5b0cf94b26940f185c3c8cb6d6bfd
Ivan Veselov <veselov.iva@gmail.com>
2018-10-22 13:46:49 +0300

    Implement applyJoins function.

--- planner/constructor.cc
+++ planner/constructor.cc
@@ -2,6 +2,7 @@
 #include <functional>
 #include <iterator>
 #include <stdexcept>
+#include <unordered_set>
 #include <unordered_map>
 #include "constructor.h"
 
@@ -160,12 +161,63 @@ static std::vector<std::unique_ptr<Operator>> applyJoins(
 	std::unordered_map<std::string, std::unique_ptr<Operator>> &tables,
 	const std::vector<AttributePredicate*> &joinPredicates
 ) {
-	// todo
+	std::unordered_set<std::string> usedTables(tables.size());
+	std::vector<bool> usedPredicates(joinPredicates.size(), false);
 
 	std::vector<std::unique_ptr<Operator>> isolatedTables;
 
 	for (std::pair<const std::string, std::unique_ptr<Operator>> &table : tables) {
-		isolatedTables.push_back(std::move(table.second));
+		if (usedTables.find(table.first) != usedTables.end()) {
+			continue;
+		}
+
+		usedTables.insert(table.first);
+		std::unique_ptr<Operator> currentRelation = std::move(table.second);
+
+		bool wasUpdated;
+		do {
+			wasUpdated = false;
+
+			for (size_t i = 0; i < joinPredicates.size(); i++) {
+				if (usedPredicates[i]) {
+					continue;
+				}
+
+				std::string leftAttribute = joinPredicates[i]->left;
+				std::string rightAttribute = joinPredicates[i]->right;
+				std::string leftTable = table_name(leftAttribute);
+				std::string rightTable = table_name(rightAttribute);
+
+				if (
+					usedTables.find(leftTable) == usedTables.end() &&
+					usedTables.find(rightTable) == usedTables.end()
+				) {
+					continue;
+				}
+
+				if (
+					usedTables.find(leftTable) != usedTables.end() &&
+					usedTables.find(rightTable) != usedTables.end()
+				) {
+					throw std::runtime_error("Cyclic dependencies in joins are not supported");
+				}
+
+				if (usedTables.find(leftTable) == usedTables.end()) {
+					std::swap(leftTable, rightTable);
+					std::swap(leftAttribute, rightAttribute);
+				}
+
+				currentRelation = std::make_unique<NLJoin>(
+					std::move(currentRelation), std::move(tables[rightTable]),
+					leftAttribute, rightAttribute
+				);
+				usedTables.insert(rightTable);
+				usedPredicates[i] = true;
+				wasUpdated = true;
+			}
+		} while (wasUpdated);
+
+		isolatedTables.push_back(std::move(currentRelation));
 	}
 
 	return isolatedTables;
